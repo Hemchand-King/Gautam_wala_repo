9d48d122dc5fd832a3cd186a367f3e0940420c69
Gwendolyn <53878781+Gwenstephanie@users.noreply.github.com>
2020-07-28 19:54:40 +0100

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -438,19 +438,19 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 except:
                     pass
                 #
-                msg = f"\nDownloading File: `{downloading_dir_name}`"
-                msg += f"\nSpeed: {file.download_speed_string()} 🔽 / {file.upload_speed_string()} 🔼"
-                msg += f"\nProgress: {file.progress_string()}"
-                msg += f"\nTotal Size: {file.total_length_string()}"
+                msg = f"\n<b>○ File:</b> `<code>{downloading_dir_name}</code>`"
+                msg += f"\n<b>○ Speed:</b> <code>{file.download_speed_string()} 🔽 / {file.upload_speed_string()} 🔼</code>"
+                msg += f"\n<b>○ Progress:</b> <code>{file.progress_string()}</code>"
+                msg += f"\n<b>○ Size:</b> <b>{file.total_length_string()}</b>"
 
                 if is_file is None :
                    msg += f"\n<b>Connections:</b> {file.connections}"
                 else :
-                   msg += f"\n<b>Info:</b>[ P : {file.connections} || S : {file.num_seeders} ]"
+                   msg += f"\n<b>○ Peers:</b> <code>{file.connections}</code> || <b>○ Seeders:</b> <code>{file.num_seeders}</code>"
 
                 # msg += f"\nStatus: {file.status}"
-                msg += f"\nETA: {file.eta_string()}"
-                msg += f"\nGID: <code>{gid}</code>"
+                msg += f"\n<b>Time Left:</b> {file.eta_string()}"
+                msg += f"\n<b>GID:</b> <code>{gid}</code>"
                 inline_keyboard = []
                 ikeyboard = []
                 ikeyboard.append(InlineKeyboardButton("Cancel 🚫", callback_data=(f"cancel {gid}").encode("UTF-8")))
