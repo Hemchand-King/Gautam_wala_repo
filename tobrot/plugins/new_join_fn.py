667018d837fe4887eb953f78907dc800ffc260d0
MaxxRider2 <66148227+MaxxRider2@users.noreply.github.com>
2020-08-07 15:52:19 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
-
+ 
 # the logging things
 import logging
 logging.basicConfig(
@@ -10,15 +10,15 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
-
+ 
 import pyrogram
-
-
+ 
+ 
 from tobrot import (
     AUTH_CHANNEL
 )
-
-
+ 
+ 
 async def new_join_f(client, message):
     chat_type = message.chat.type
     if chat_type != "private":
@@ -30,17 +30,17 @@ async def new_join_f(client, message):
         )
     # delete all other messages, except for AUTH_CHANNEL
     await message.delete(revoke=True)
-
-
+ 
+ 
 async def help_message_f(client, message):
     # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""Welcome To @LeechZone 😎 \n\n <b><u>I Can Leech Files To Telegram & Gdrive</u></b> \n\n Do One Leech & Wait For It To Complete Before Doing Another \n\n <a href="https://t.me/LeechZone/10966">⚠ READ GROUP PINNED MESSAGE RULES ⚠</a>""", disable_web_page_preview=True) 
-
-
+    await message.reply_text(""" """)
+ 
+ 
 async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
